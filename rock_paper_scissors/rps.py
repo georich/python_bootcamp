628c1b1ef358c677832b18c768be83b6389f3800
George Richards <g.richards101@gmail.com>
2018-02-24 14:43:18 +0000

    initial refactoring

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -4,17 +4,20 @@ player2 = input("Player 2, enter your choice: ")
 
 if player1 == player2:
     print("A draw!")
-elif player1 == "rock" and player2 == "scissors":
-    print("Player 1 wins!")
-elif player1 == "rock" and player2 == "paper":
-    print("Player 2 wins!")
-elif player1 == "paper" and player2 == "rock":
-    print("Player 1 wins!")
-elif player1 == "paper" and player2 == "scissors":
-    print("Player 2 wins!")
-elif player1 == "scissors" and player2 == "rock":
-    print("Player 2 wins!")
-elif player1 == "scissors" and player2 == "paper":
-    print("Player 1 wins!")
+elif player1 == "rock":
+    if player2 == "scissors":
+        print("Player 1 wins!")
+    elif player2 == "paper":
+        print("Player 2 wins!")
+elif player1 == "paper":
+    if player2 == "rock":
+        print("Player 1 wins!")
+    elif player2 == "scissors":
+        print("Player 2 wins!")
+elif player1 == "scissors":
+    if player2 == "rock":
+        print("Player 2 wins!")
+    elif player2 == "paper":
+        print("Player 1 wins!")
 else:
     print("Something has gone wrong, please try again")
