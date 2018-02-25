3a6e0da8852f147b27319ce643a89f7519771662
George Richards <g.richards101@gmail.com>
2018-02-25 21:35:01 +0000

    more simple problems

---
+++ loops/emoji_art.py
@@ -0,0 +1,6 @@
+final_line = 10
+count = 1
+
+while count <= final_line:
+    print("\U0001f600"*count)
+    count += 1
---
+++ loops/mimic.py
@@ -0,0 +1,7 @@
+message = input("Hey, how's it going? ")
+
+while message != "stop copying me":
+    print(message)
+    message = input()
+
+print("Fine! I'll be quiet now")
