1e9d8f809842adfbece2bef7e9fecbba9509110e
George Richards <g.richards101@gmail.com>
2018-02-26 19:09:17 +0000

    add replayability

--- guessing_game/guessing_game.py
+++ guessing_game/guessing_game.py
@@ -1,13 +1,17 @@
 from random import randint
 
-number = randint(1, 10)
-guess = None
+playon = None
 
-while guess != number:
-    guess = int(input("Guess a number between 1 and 10: "))
-    if guess == number:
-        print("Correct!")
-    elif guess < number:
-        print("You guessed too low, try again!")
-    else:
-        print("You guessed too high, try again!")
+while playon != "n":
+    number = randint(1, 10)
+    guess = None
+
+    while guess != number:
+        guess = int(input("Guess a number between 1 and 10: "))
+        if guess == number:
+            print("Correct!")
+            playon = input("Do you want to keep playing? y/n ")
+        elif guess < number:
+            print("You guessed too low, try again!")
+        else:
+            print("You guessed too high, try again!")
