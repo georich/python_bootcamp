94840bdf88e260364d9eed148883638e809443f8
George Richards <g.richards101@gmail.com>
2018-02-26 21:44:14 +0000

    simple list comp problems

---
+++ lists/names_and_even.py
@@ -0,0 +1,7 @@
+names = ["Elie", "Tim", "Matt"]
+answer = [str[0] for str in names]
+print(answer)
+
+numbers = [1, 2, 3, 4, 5, 6]
+answer2 = [num for num in numbers if num % 2 == 0]
+print(answer2)
