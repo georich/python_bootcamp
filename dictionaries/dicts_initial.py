7a1152860858080216e54f8995f16d23b72ab463
George Richards <g.richards101@gmail.com>
2018-02-26 22:23:09 +0000

    initial dictionary understanding

---
+++ dictionaries/dicts_initial.py
@@ -0,0 +1,24 @@
+#instructor = ["Colt", True, 4, "Python", False] # Not enough information to discern
+
+# Can get this information using a dictionary
+instructor = {
+    "name": "Colt",
+    "owns_dog": True,
+    "num_courses": 4,
+    "favourite_language": "Python",
+    "is_hilarious": False
+}
+
+# keys almost always numbers or strings
+cat = {
+    "name": "Minnie",
+    "age": 8,
+    "fat": True
+}
+print(cat["name"])
+
+user_info = {
+    "name": "George",
+    "age": 23,
+    "height": 1.72
+}
