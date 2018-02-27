eb8fecadffacb4c399a582e959c23dda376954b3
George Richards <g.richards101@gmail.com>
2018-02-27 17:52:34 +0000

    state abbreviations problem

---
+++ dictionaries/state_abbreviations.py
@@ -0,0 +1,8 @@
+list1 = ["CA", "NJ", "RI"]
+list2 = ["California", "New Jersey", "Rhode Island"]
+
+#answer = {}
+#for i in range(0, 3):
+#    answer[list1[i]] = list2[i]
+#print(answer)
+answer = {list1[i]: list2[i] for i in range(0, 3)}
