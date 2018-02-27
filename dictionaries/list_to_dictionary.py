84ba8a799b186952c7602d261a10219c7c53cbbc
George Richards <g.richards101@gmail.com>
2018-02-27 17:57:30 +0000

    list to dict refactored

--- dictionaries/list_to_dictionary.py
+++ dictionaries/list_to_dictionary.py
@@ -1,4 +1,5 @@
 person = [["name", "Jared"], ["job", "Musician"], ["city", "Bern"]]
 
-answer = {person[i][0]: person[i][1] for i in range(0, 3)}
+#answer = {person[i][0]: person[i][1] for i in range(0, 3)}
+answer = dict(person)
 print(answer)
