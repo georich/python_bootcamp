d73da1d15baa79889247c7563d8be3f772c96c77
George Richards <g.richards101@gmail.com>
2018-02-27 22:17:03 +0000

    tuple understanding

---
+++ tuples_and_sets/tuple.py
@@ -0,0 +1,7 @@
+numbers = (1, 2, 3 ,4) # immutable
+print(type(numbers))
+# faster than lists
+# makes code more robust
+# valid keys in a dict
+count_ones = numbers.count(1)
+index_of_two = numbers.index(2) # will only give first matching index
