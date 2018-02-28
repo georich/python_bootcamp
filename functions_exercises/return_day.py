b64050576e7882094f0e8ffb40f6e5bebad67a07
George Richards <g.richards101@gmail.com>
2018-02-28 13:19:52 +0000

    return_day problem

---
+++ functions_exercises/return_day.py
@@ -0,0 +1,11 @@
+def return_day(num):
+    days = {
+        1: "Sunday",
+        2: "Monday",
+        3: "Tuesday",
+        4: "Wednesday",
+        5: "Thursday",
+        6: "Friday",
+        7: "Saturday"
+    }
+    return days.get(num)
