ee92e1b13c26b56186341d355405dda8a296be55
George Richards <g.richards101@gmail.com>
2018-02-28 16:36:47 +0000

    initial function *args understanding

---
+++ functions_II/*args.py
@@ -0,0 +1,13 @@
+# *arg, special operator to pass to functions
+# will gather remaining arguments as a tuple
+
+def sum_all_nums(num1, num2, num3):
+    return num1 + num2 + num3
+
+def sum_all_nums(*args):
+    total = 0
+    for num in args:
+        total += num
+    return total
+
+print(sum_all_nums(4, 6, 9))
