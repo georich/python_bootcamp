27af5e0866290972514f080371053110d48bf28b
George Richards <g.richards101@gmail.com>
2018-03-03 17:46:13 +0000

    error handling exercise solution

---
+++ debugging_error_handling/deh_exercise.py
@@ -0,0 +1,12 @@
+def divide(num1, num2):
+    try:
+        result = num1 / num2
+    except TypeError:
+        return "Please provide two integers or floats"
+    except ZeroDivisionError:
+        return "Please do not divide by zero"
+    return result
+
+print(divide(4,2))
+print(divide([],"1"))
+print(divide(1,0))
