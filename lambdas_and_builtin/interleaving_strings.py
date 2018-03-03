d5f3800111f483807f75b44279da95e8584e43a0
George Richards <g.richards101@gmail.com>
2018-03-03 15:13:18 +0000

    interleaved string refactor

--- lambdas_and_builtin/interleaving_strings.py
+++ lambdas_and_builtin/interleaving_strings.py
@@ -1,8 +1,5 @@
 def interleave(string1, string2):
-    zipped_strings = list(zip(string1, string2))
-    joined2 = ["".join(part) for part in zipped_strings]
-    final_string = "".join(joined2)
-    return final_string
+    return "".join("".join(part) for part in zip(string1, string2))
 
 print(interleave("hi", "ha"))
 print(interleave("sna", "udy"))
