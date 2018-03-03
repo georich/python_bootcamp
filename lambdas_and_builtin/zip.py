443e7ee3906f7906a1abc09be4768685c0e58b67
George Richards <g.richards101@gmail.com>
2018-03-03 14:57:11 +0000

    further zip methods

--- lambdas_and_builtin/zip.py
+++ lambdas_and_builtin/zip.py
@@ -4,3 +4,21 @@ students = ["dan", "ang", "kate"]
 
 final_grades = [max(pair) for pair in zip(midterms, finals)]
 print(final_grades)
+
+final_grades_dict = {value[0]: max(value[1], value[2]) for value in zip(students, midterms, finals)}
+print(final_grades_dict)
+
+scores = map(
+    lambda pair: max(pair),
+    zip(midterms, finals)
+)
+print(list(scores))
+
+scores_extra = zip(
+    students,
+    map(
+        lambda pair: max(pair),
+        zip(midterms, finals)
+    )
+)
+print(dict(scores_extra))
