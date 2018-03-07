f217ae5dc6d87c63a8d2239f860b2e20cddaf1ad
George Richards <g.richards101@gmail.com>
2018-03-07 23:35:30 +0000

    wrapping with functools

--- decorators/metadata.py
+++ decorators/metadata.py
@@ -1,4 +1,7 @@
+from functools import wraps
+
 def log_function_data(fn):
+    @wraps(fn)
     def wrapper(*args, **kwargs):
         """wrapper function"""
         print(f"You are about to call {fn.__name__}")
@@ -12,3 +15,5 @@ def add(x, y):
     return x + y
 
 print(add(10, 20))
+print(add.__doc__)
+print(add.__name__)
