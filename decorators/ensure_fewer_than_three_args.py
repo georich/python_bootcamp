c27c177fd3c029d97e616f2d29831c17b2eb2a9a
George Richards <g.richards101@gmail.com>
2018-03-08 19:35:55 +0000

    ensuring fewer than 3 args solution

---
+++ decorators/ensure_fewer_than_three_args.py
@@ -0,0 +1,19 @@
+from functools import wraps
+
+def ensure_fewer_than_three_args(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        if len(args) >= 3:
+            return "Too many arguments!"
+        return fn(*args, **kwargs)
+    return wrapper
+
+@ensure_fewer_than_three_args
+def add_all(*nums):
+    return sum(nums)
+
+print(add_all())
+print(add_all(1))
+print(add_all(1, 2))
+print(add_all(1, 2, 3))
+print(add_all(1, 2, 3, 4))
