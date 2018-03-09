8e2af77c85cada8ff617ee6b63f7919b2cd921b1
George Richards <g.richards101@gmail.com>
2018-03-09 22:02:40 +0000

    added doctests to number_compare function

--- functions_exercises/number_compare.py
+++ functions_exercises/number_compare.py
@@ -1,4 +1,12 @@
 def number_compare(a, b):
+    """
+    >>> number_compare(1, 1)
+    'Numbers are equal'
+    >>> number_compare(2, 1)
+    'First is greater'
+    >>> number_compare(1, 2)
+    'Second is greater'
+    """
     if a == b:
         return "Numbers are equal"
     elif a > b:
