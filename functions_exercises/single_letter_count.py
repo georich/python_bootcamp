44882ecbbe95b422d9b098d90ce3a0d62f1b589c
George Richards <g.richards101@gmail.com>
2018-03-09 22:04:28 +0000

    added doctests to single_letter_count function

--- functions_exercises/single_letter_count.py
+++ functions_exercises/single_letter_count.py
@@ -1,2 +1,10 @@
 def single_letter_count(phrase, letter):
+    """
+    >>> single_letter_count('Hello', 'l')
+    2
+    >>> single_letter_count('aeiou', 'm')
+    0
+    >>> single_letter_count('Capital', 'c')
+    1
+    """
     return phrase.lower().count(letter.lower())
