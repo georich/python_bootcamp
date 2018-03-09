0bd119a69f0eab6ae02c2e04ae3fe6a4dc634061
George Richards <g.richards101@gmail.com>
2018-03-09 22:10:30 +0000

    added doctest to multiple_letter_count function

--- functions_exercises/multiple_letter_count.py
+++ functions_exercises/multiple_letter_count.py
@@ -1,4 +1,10 @@
 def multiple_letter_count(phrase):
+    """
+    >>> multiple_letter_count('awesome')
+    {'a': 1, 'w': 1, 'e': 2, 's': 1, 'o': 1, 'm': 1}
+    >>> multiple_letter_count('AAAAAAH!')
+    {'A': 6, 'H': 1, '!': 1}
+    """
     return {letter: phrase.count(letter) for letter in phrase}
 
 print(multiple_letter_count("awesome"))
