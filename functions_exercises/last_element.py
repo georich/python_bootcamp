5e15fa426b3932af93018bf609fdf09a662f47b4
George Richards <g.richards101@gmail.com>
2018-03-09 22:00:51 +0000

    added doctests to last_element function

--- functions_exercises/last_element.py
+++ functions_exercises/last_element.py
@@ -1,4 +1,14 @@
 def last_element(values):
+    """
+    >>> last_element([]) is None
+    True
+    >>> last_element([1, 2, 3, 4])
+    4
+    >>> last_element([1])
+    1
+    >>> last_element([1, 2, 3, '4'])
+    '4'
+    """
     if len(values) == 0:
         return None
     return values[-1]
