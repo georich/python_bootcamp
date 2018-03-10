92cdd1fc5d0c770426f45c3a804d27f1e22052b3
George Richards <g.richards101@gmail.com>
2018-03-10 13:04:18 +0000

    added doctests to capitalize function

--- functions_exercises/capitalize.py
+++ functions_exercises/capitalize.py
@@ -1,4 +1,10 @@
 def capitalize(word):
+    """
+    >>> capitalize("george")
+    'George'
+    >>> capitalize("George")
+    'George'
+    """
     return f"{word[0].upper()}{word[1::]}"
 
 print(capitalize("george"))
