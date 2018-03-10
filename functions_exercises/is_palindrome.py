bd911b6c0a32771d9b7c45f8370d27bd690f74fa
George Richards <g.richards101@gmail.com>
2018-03-10 13:00:58 +0000

    added doctests to is palindrome function

--- functions_exercises/is_palindrome.py
+++ functions_exercises/is_palindrome.py
@@ -1,3 +1,13 @@
 def is_palindrome(word):
+    """
+    >>> is_palindrome("")
+    False
+    >>> is_palindrome("a")
+    True
+    >>> is_palindrome("aabaa")
+    True
+    >>> is_palindrome("abbaa")
+    False
+    """
     reduced = word.lower().replace(" ", "")
     return reduced == reduced[::-1]
