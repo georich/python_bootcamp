cd6dc1e5e07a5408d5f5158e5de8a56bfc6aa1f3
George Richards <g.richards101@gmail.com>
2018-03-11 13:11:34 +0000

    understanding of cursor, seeking and closing

--- file_IO/reading_files.py
+++ file_IO/reading_files.py
@@ -1,2 +1,12 @@
 file = open("story.txt")
-file.read()
+print(file.read())
+
+print(file.read())
+file.seek(0) # will send cursor back to beginning
+
+print(file.read())
+
+file.seek(0)
+print(file.readlines()) # puts each line in a seperate string in a list
+file.close()
+print(file.closed)
