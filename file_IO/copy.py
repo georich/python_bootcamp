61a6bfa7442f3662cfd15841fd6e0759451c9dae
George Richards <g.richards101@gmail.com>
2018-03-11 15:31:06 +0000

    less indented solution

--- file_IO/copy.py
+++ file_IO/copy.py
@@ -1,4 +1,8 @@
 def copy(first_file, second_file):
     with open(first_file) as original:
-        with open(second_file, "w") as to_copy:
-            to_copy.write(original.read())
+        # with open(second_file, "w") as to_copy:
+        #     to_copy.write(original.read())
+        text = original.read()
+    
+    with open(second_file, "w") as to_copy:
+        to_copy.write(text)
