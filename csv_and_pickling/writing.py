92c988e234ba7758f8a7e1f4cfca6f6752b23160
George Richards <g.richards101@gmail.com>
2018-03-24 22:36:54 +0000

    using DictWriter understanding

--- csv_and_pickling/writing.py
+++ csv_and_pickling/writing.py
@@ -1,8 +1,18 @@
-from csv import writer
+from csv import writer, DictWriter
 
-with open("writing_test.csv", "w") as file:
-    csv_writer = writer(file)
-    csv_writer.writerow(["Name", "Colour"])
-    csv_writer.writerow(["Minnie", "Ginger"])
-    csv_writer.writerow(["Alex", "Black and White"])
-    csv_writer.writerow(["TJ", "Ginger"])
+# with open("writing_test.csv", "w") as file:
+#     csv_writer = writer(file)
+#     csv_writer.writerow(["Name", "Colour"])
+#     csv_writer.writerow(["Minnie", "Ginger"])
+#     csv_writer.writerow(["Alex", "Black and White"])
+#     csv_writer.writerow(["TJ", "Ginger"])
+
+with open("cats.csv", "w") as file:
+    headers = ["Name", "Breed", "Age"]
+    csv_writer = DictWriter(file, fieldnames=headers)
+    csv_writer.writeheader()
+    csv_writer.writerow({
+        "Name": "Minnie",
+        "Breed": "Orange Tabby",
+        "Age": 9
+    })
