5b9bada2b8a7ac221c04f78b52b8fae3e25390eb
George Richards <georich@protonmail.com>
2018-05-06 18:38:03 +0100

    Slight rearrange

--- csv_and_pickling/reading.py
+++ csv_and_pickling/reading.py
@@ -1,12 +1,11 @@
 from csv import reader
+from csv import DictReader
 
 with open("trees.csv") as file:
-    csv_reader = reader(file) # this is an iterator
-    next(csv_reader) # this moves the cursor passed the headers
+    csv_reader = reader(file)  # this is an iterator
+    next(csv_reader)  # this moves the cursor passed the headers
     for row in csv_reader:
         print(row)
-
-from csv import DictReader
 
 with open("trees.csv") as file:
     csv_reader = DictReader(file)
